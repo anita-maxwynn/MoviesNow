a2e92ccdb326e02aaf9f2ba4c2abb37b9e2d093c
anita-maxwynn <cse22100@gmail.com>
2025-08-22 13:15:59 +0530

    Remove meet folder

--- meet/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MeetConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meet'
-
-    def ready(self):
-        import meet.signals
